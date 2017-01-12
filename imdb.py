97a64a17129f844f14a1227781569f45ec7be5fa
MattScicluna <mattcscicluna@gmail.com>
2017-01-12 09:40:02 -0500

    improved pep8 styling

--- imdb.py
+++ imdb.py
@@ -1,7 +1,7 @@
 import urllib.request
 from bs4 import BeautifulSoup
 import csv
-import argparse, sys, random, re
+import argparse, random, re
 from math import log
 from math import ceil
 
@@ -76,7 +76,7 @@ def run(movie, max_movies):
         soup = BeautifulSoup(pagedat)
 
     # The [:-7] removes the " - IMDB" after the title
-        title=soup.title.text[:-7]
+        title = soup.title.text[:-7]
         year = title[-5:-1]
         if not year.isdigit(): year = ''  # movie not released
         # find more movie details
@@ -103,22 +103,22 @@ def run(movie, max_movies):
 
             try:
                 if 'USA' in details.find(text='Gross:').parent.next_sibling.next_sibling.get_text().strip():
-                  gross = details.find(text='Gross:').parent.next_sibling.strip()
+                    gross = details.find(text='Gross:').parent.next_sibling.strip()
 
             except Exception:
                 print('No gross')
 
             try:
-               genre = storyline.find(text="Genres:").find_next().get_text().lstrip()
+                genre = storyline.find(text="Genres:").find_next().get_text().lstrip()
 
             except Exception:
                 print('No genre')
 
             try:
-               keywordsl = storyline.find(text="Plot Keywords:").parent.find_next_siblings()
-               keywords1 = keywordsl[0].get_text().lstrip()
-               keywords2 = keywordsl[2].get_text().lstrip()
-               keywords3 = keywordsl[4].get_text().lstrip()
+                keywordsl = storyline.find(text="Plot Keywords:").parent.find_next_siblings()
+                keywords1 = keywordsl[0].get_text().lstrip()
+                keywords2 = keywordsl[2].get_text().lstrip()
+                keywords3 = keywordsl[4].get_text().lstrip()
 
             except Exception:
                 print('No keywords')
@@ -160,12 +160,12 @@ def run(movie, max_movies):
 
         for row in allrows[1:]:
             if 'castlist_label' in row.td.get('class'):
-                break # no more actors
+                break  # no more actors
 
-            links=row.find_all('a')
-            link=links[1].get('href')
-            actor=row.get_text()
-            href = 'http://www.imdb.com/filmosearch?explore=title_type&role='+\
+            links = row.find_all('a')
+            link = links[1].get('href')
+            actor = row.get_text()
+            href = 'http://www.imdb.com/filmosearch?explore=title_type&role=' +\
                    link[6:15]+'&ref_=filmo_vw_smp&sort=year,desc&mode=simple&page=1&title_type=movie'
             actor_links.append(href)
             actor_names.append(actor)
@@ -174,7 +174,7 @@ def run(movie, max_movies):
         # There might be fewer than five actors
 
         if len(actor_links) < 5:
-            choose = random.randint(0, len(actor_links) -1)
+            choose = random.randint(0, len(actor_links) - 1)
         else:
             choose = choose_weights(WEIGHTS)
         href = actor_links[choose]
@@ -187,23 +187,23 @@ def run(movie, max_movies):
             g_csv.writerow([str(index), title, firstName, lastName])
 
         except Exception:
-            pass # some titles have strange characters
+            pass  # some titles have strange characters
 
         urllines = urllib.request.urlopen(href)
         pagedat = urllines.read()
         urllines.close()
         soup = BeautifulSoup(pagedat)
-        links=soup.find_all('a')
-        movies=[]
-        for i in range(1,len(links)):
+        links = soup.find_all('a')
+        movies = []
+        for i in range(1, len(links)):
             if 'title' in str(links[i]):
                 if 'film' in str(links[i]):
                     movies.append('http://www.imdb.com' + links[i].get('href'))
         if len(movies) <= 1: continue
         if len(movies) < 5:
-            choose2 = random.randint(0, len(movies) -1)
+            choose2 = random.randint(0, len(movies) - 1)
         else:
-            choose2 = min(choose_movie(ceil(len(movies)/4)),len(movies)-1)
+            choose2 = min(choose_movie(ceil(len(movies)/4)), len(movies) - 1)
         movie = movies[choose2]
 
         print(movie)
